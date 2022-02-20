58db63b4247587f4ab0c9379da383bab914416f3
SeeeSaww <pyaesoneaung1126@hotmail.com>
2022-02-20 20:51:44 +0630

    netlift_toml

--- src/index.tsx
+++ src/index.tsx
@@ -2,22 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import App from "./App"
-import Register from "./components/register"
-import Login from "./components/login"
-import Reset from "./components/forgetpw"
-import RestFrom from "./components/restfrom"
+import App from "./App";
+import Register from "./components/register";
+import Login from "./components/login";
+import Reset from "./components/forgetpw";
+import RestFrom from "./components/restfrom";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/restpw" element={<Reset />} />
-          <Route path="/restpw/:idTk" element={<RestFrom />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/restpw" element={<Reset />} />
+        <Route path="/restpw/:idTk" element={<RestFrom />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
