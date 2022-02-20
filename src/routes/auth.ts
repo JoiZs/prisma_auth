bd43dfe774d1e944a4665d4e47dae1f60c2e8006
SeeeSaww <pyaesoneaung1126@hotmail.com>
2022-02-20 21:17:38 +0630

    crossOrg

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -74,13 +74,13 @@ authRoute.post("/login", async (req, res) => {
   res.cookie("actk_id", accToken, {
     maxAge: 1000 * 60 * 3,
     httpOnly: true,
-    sameSite: "none",
+    sameSite: "lax",
     secure: true,
   });
   res.cookie("rftk_id", refToken, {
     maxAge: 1000 * 60 * 60 * 24 * 365,
     httpOnly: true,
-    sameSite: "none",
+    sameSite: "lax",
     secure: true,
   });
 
